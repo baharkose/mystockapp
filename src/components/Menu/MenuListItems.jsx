17a2f352e46fc4af4ddd6f0729b9da22f228c5da
baharkose <baharkse17@gmail.com>
2024-05-13 20:31:41 +0300

    feat: add styling

--- src/components/Menu/MenuListItems.jsx
+++ src/components/Menu/MenuListItems.jsx
@@ -65,7 +65,7 @@ const MenuListItems = () => {
               <li key={index} onClick={() => navigate(item.url)}>
                 <a
                   href={item.link}
-                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 hover:text-sky-800 dark:hover:bg-gray-700 group"
                 >
                   {item.icon}
                   {/* Dinamik olarak render */}
